befc20cefe929fc6336c75efcd4a3ad94e852e05
hjiyu1965 <hjiyu1965@users.noreply.github.com>
2026-08-09 06:32:31 +0000

    fix: remove libion.so (AOSP conflict), add libminuivivo.so rule, fix fstab comment

--- device.mk
+++ device.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/system/lib64/libwifikeystorehalext.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libwifikeystorehalext.so \
     $(LOCAL_PATH)/recovery/root/system/lib64/libqcbor.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libqcbor.so \
     $(LOCAL_PATH)/recovery/root/system/lib64/libqtikeymaster4.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libqtikeymaster4.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libminuivivo.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libminuivivo.so \
     $(LOCAL_PATH)/recovery/root/system/lib64/libkeymasterdeviceutils.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libkeymasterdeviceutils.so \
     $(LOCAL_PATH)/recovery/root/system/lib64/libsqlite.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libsqlite.so \
     $(LOCAL_PATH)/recovery/root/system/lib64/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libandroidicu.so \
