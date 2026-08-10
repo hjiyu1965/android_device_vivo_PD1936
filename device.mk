40a482cf2be929f647715b025c401bd912ad1369
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-08-10 21:36:39 +0800

    fix: add vivo vivofbe fallback for CE decrypt, add fstab.qcom for vivo fstab compat

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ LOCAL_PATH := device/vivo/PD1936
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY_ROOT)/etc/recovery.fstab \
     $(LOCAL_PATH)/recovery/root/etc/fstab:$(TARGET_COPY_OUT_RECOVERY_ROOT)/etc/fstab \
+    $(LOCAL_PATH)/recovery/root/etc/fstab:$(TARGET_COPY_OUT_RECOVERY_ROOT)/fstab.qcom \
     $(LOCAL_PATH)/recovery/root/init.recovery.platform.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.platform.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.qcom.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.svc.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.svc.rc \
