4f09d75eda98b75a70b1220d0bd4c41f9bf1fb46
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-08-11 07:43:50 +0800

    Update PRODUCT_COPY_FILES paths in device.mk

--- device.mk
+++ device.mk
@@ -12,87 +12,87 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_RECOVERY_ROOT_OUT)/etc/recovery.fstab \
     $(LOCAL_PATH)/recovery/root/etc/fstab:$(TARGET_RECOVERY_ROOT_OUT)/etc/fstab \
     $(LOCAL_PATH)/recovery/root/etc/fstab:$(TARGET_RECOVERY_ROOT_OUT)/fstab.qcom \
-    $(LOCAL_PATH)/recovery/root/init.recovery.platform.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.platform.rc \
-    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.qcom.rc \
-    $(LOCAL_PATH)/recovery/root/init.recovery.svc.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.svc.rc \
-    $(LOCAL_PATH)/recovery/root/init.recovery.touch.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.touch.rc \
-    $(LOCAL_PATH)/recovery/root/ueventd.qcom.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/ueventd.qcom.rc
+    $(LOCAL_PATH)/recovery/root/init.recovery.platform.rc:$(TARGET_RECOVERY_ROOT_OUT)/init.recovery.platform.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:$(TARGET_RECOVERY_ROOT_OUT)/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.svc.rc:$(TARGET_RECOVERY_ROOT_OUT)/init.recovery.svc.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.touch.rc:$(TARGET_RECOVERY_ROOT_OUT)/init.recovery.touch.rc \
+    $(LOCAL_PATH)/recovery/root/ueventd.qcom.rc:$(TARGET_RECOVERY_ROOT_OUT)/ueventd.qcom.rc
 
 # vivo proprietary binaries
 # NOTE: wait_for_keymaster is built from source by AOSP/TWRP, do NOT copy as prebuilt
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/system/bin/aria2c:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/bin/aria2c \
-    $(LOCAL_PATH)/recovery/root/system/bin/guardianangle:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/bin/guardianangle \
-    $(LOCAL_PATH)/recovery/root/system/bin/little_buddy:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/bin/little_buddy \
-    $(LOCAL_PATH)/recovery/root/system/bin/vivofbe:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/bin/vivofbe \
-    $(LOCAL_PATH)/recovery/root/system/bin/vivotool:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/bin/vivotool \
-    $(LOCAL_PATH)/recovery/root/system/bin/vts_app_recovery:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/bin/vts_app_recovery
+    $(LOCAL_PATH)/recovery/root/system/bin/aria2c:$(TARGET_RECOVERY_ROOT_OUT)/system/bin/aria2c \
+    $(LOCAL_PATH)/recovery/root/system/bin/guardianangle:$(TARGET_RECOVERY_ROOT_OUT)/system/bin/guardianangle \
+    $(LOCAL_PATH)/recovery/root/system/bin/little_buddy:$(TARGET_RECOVERY_ROOT_OUT)/system/bin/little_buddy \
+    $(LOCAL_PATH)/recovery/root/system/bin/vivofbe:$(TARGET_RECOVERY_ROOT_OUT)/system/bin/vivofbe \
+    $(LOCAL_PATH)/recovery/root/system/bin/vivotool:$(TARGET_RECOVERY_ROOT_OUT)/system/bin/vivotool \
+    $(LOCAL_PATH)/recovery/root/system/bin/vts_app_recovery:$(TARGET_RECOVERY_ROOT_OUT)/system/bin/vts_app_recovery
 
 # FBE decryption chain - vendor/bin HAL services
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/bin/qseecomd:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/bin/qseecomd \
-    $(LOCAL_PATH)/recovery/root/vendor/bin/android.hardware.keymaster@4.0-service-qti:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/bin/android.hardware.keymaster@4.0-service-qti \
-    $(LOCAL_PATH)/recovery/root/vendor/bin/android.hardware.gatekeeper@1.0-service-qti:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/bin/android.hardware.gatekeeper@1.0-service-qti
+    $(LOCAL_PATH)/recovery/root/vendor/bin/qseecomd:$(TARGET_RECOVERY_ROOT_OUT)/vendor/bin/qseecomd \
+    $(LOCAL_PATH)/recovery/root/vendor/bin/android.hardware.keymaster@4.0-service-qti:$(TARGET_RECOVERY_ROOT_OUT)/vendor/bin/android.hardware.keymaster@4.0-service-qti \
+    $(LOCAL_PATH)/recovery/root/vendor/bin/android.hardware.gatekeeper@1.0-service-qti:$(TARGET_RECOVERY_ROOT_OUT)/vendor/bin/android.hardware.gatekeeper@1.0-service-qti
 
 # vivo proprietary libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libGuardianAngleClient.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libGuardianAngleClient.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libGuardianAngleService.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libGuardianAngleService.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libGuardianAngleServiceImpl.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libGuardianAngleServiceImpl.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libbigdata_utils.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libbigdata_utils.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libvivofscrypt.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libvivofscrypt.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libvivogatekeeper.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libvivogatekeeper.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libwifikeystorehalext.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libwifikeystorehalext.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libqcbor.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libqcbor.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libqtikeymaster4.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libqtikeymaster4.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libminuivivo.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libminuivivo.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libkeymasterdeviceutils.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libkeymasterdeviceutils.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/librpmb.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/librpmb.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libsqlite.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libsqlite.so \
-    $(LOCAL_PATH)/recovery/root/system/lib64/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/lib64/libandroidicu.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterutils.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libkeymasterutils.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libion.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libion.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libGuardianAngleClient.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libGuardianAngleClient.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libGuardianAngleService.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libGuardianAngleService.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libGuardianAngleServiceImpl.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libGuardianAngleServiceImpl.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libbigdata_utils.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libbigdata_utils.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libvivofscrypt.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libvivofscrypt.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libvivogatekeeper.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libvivogatekeeper.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libwifikeystorehalext.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libwifikeystorehalext.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libqcbor.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libqcbor.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libqtikeymaster4.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libqtikeymaster4.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libminuivivo.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libminuivivo.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libkeymasterdeviceutils.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libkeymasterdeviceutils.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/librpmb.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/librpmb.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libsqlite.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libsqlite.so \
+    $(LOCAL_PATH)/recovery/root/system/lib64/libandroidicu.so:$(TARGET_RECOVERY_ROOT_OUT)/system/lib64/libandroidicu.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterutils.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libkeymasterutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libion.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libion.so \
 
 # Qualcomm vendor proprietary libraries
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/etc/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/etc/gpfspath_oem_config.xml \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libGPreqcancel.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libGPreqcancel.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libGPreqcancel_svc.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libGPreqcancel_svc.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libQSEEComAPI.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libQSEEComAPI.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libStDrvInt.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libStDrvInt.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdiag.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libdiag.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdrmfs.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libdrmfs.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdrmtime.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libdrmtime.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdsutils.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libdsutils.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libidl.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libidl.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libjson.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libjson.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libmdmdetect.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libmdmdetect.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libperipheral_client.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libperipheral_client.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqisl.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqisl.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_cci.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqmi_cci.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_client_qmux.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqmi_client_qmux.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_common_so.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqmi_common_so.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_csi.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqmi_csi.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_encdec.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqmi_encdec.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmiservices.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqmiservices.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqrtr.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqrtr.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqsocket.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libqsocket.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libsecureui.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libsecureui.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libsecureui_svcsock.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libsecureui_svcsock.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libspcom.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libspcom.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libssd.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libssd.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libssl.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libssl.so \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/libtime_genoff.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/libtime_genoff.so
+    $(LOCAL_PATH)/recovery/root/vendor/etc/gpfspath_oem_config.xml:$(TARGET_RECOVERY_ROOT_OUT)/vendor/etc/gpfspath_oem_config.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libGPreqcancel.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libGPreqcancel.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libGPreqcancel_svc.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libGPreqcancel_svc.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libQSEEComAPI.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libQSEEComAPI.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libStDrvInt.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libStDrvInt.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdiag.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libdiag.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdrmfs.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libdrmfs.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdrmtime.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libdrmtime.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libdsutils.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libdsutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libidl.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libidl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libjson.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libjson.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libmdmdetect.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libmdmdetect.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libperipheral_client.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libperipheral_client.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqisl.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqisl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_cci.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqmi_cci.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_client_qmux.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqmi_client_qmux.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_common_so.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqmi_common_so.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_csi.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqmi_csi.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmi_encdec.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqmi_encdec.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqmiservices.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqmiservices.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqrtr.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqrtr.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqsocket.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libqsocket.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libsecureui.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libsecureui.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libsecureui_svcsock.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libsecureui_svcsock.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libspcom.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libspcom.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libssd.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libssd.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libssl.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libssl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libtime_genoff.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/libtime_genoff.so
 
 # Touchscreen firmware
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x0028.bin:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x0028.bin \
-    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x002C.bin:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x002C.bin \
-    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x502100028.bin:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x502100028.bin \
-    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x50213002C.bin:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x50213002C.bin \
-    $(LOCAL_PATH)/recovery/root/vendor/firmware/bdwlan.bin.4g:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/firmware/bdwlan.bin.4g \
-    $(LOCAL_PATH)/recovery/root/vendor/firmware/touch_firmwares_recovery.bin:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/firmware/touch_firmwares_recovery.bin
+    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x0028.bin:$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x0028.bin \
+    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x002C.bin:$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/TP-CONFIG-FW-PD1936-LCMID33-VER0x002C.bin \
+    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x502100028.bin:$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x502100028.bin \
+    $(LOCAL_PATH)/recovery/root/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x50213002C.bin:$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/TP-FW-PD1936-LCMID33-VER0x50213002C.bin \
+    $(LOCAL_PATH)/recovery/root/vendor/firmware/bdwlan.bin.4g:$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/bdwlan.bin.4g \
+    $(LOCAL_PATH)/recovery/root/vendor/firmware/touch_firmwares_recovery.bin:$(TARGET_RECOVERY_ROOT_OUT)/vendor/firmware/touch_firmwares_recovery.bin
 
 # Device properties for touchscreen firmware matching and FBE decryption
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -120,8 +120,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # VINTF manifest for keymaster/gatekeeper HAL registration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/etc/vintf/manifest.xml:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/etc/vintf/manifest.xml
+    $(LOCAL_PATH)/recovery/root/vendor/etc/vintf/manifest.xml:$(TARGET_RECOVERY_ROOT_OUT)/vendor/etc/vintf/manifest.xml
 
 # FBE decryption - QCOM TUI communication library (NOT built by AOSP)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so:$(TARGET_COPY_OUT_RECOVERY_ROOT)/vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so:$(TARGET_RECOVERY_ROOT_OUT)/vendor/lib64/vendor.qti.hardware.tui_comm@1.0.so
