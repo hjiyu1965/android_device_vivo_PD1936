dff4a7644272d25f33e6198ccbd0d207b747d884
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-08-10 13:57:34 +0800

    fix: add Android-format /etc/fstab for ReadDefaultFstab in FBE decrypt

--- device.mk
+++ device.mk
@@ -10,6 +10,7 @@ LOCAL_PATH := device/vivo/PD1936
 # Init scripts and configuration files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY_ROOT)/system/etc/recovery.fstab \
+    $(LOCAL_PATH)/recovery/root/etc/fstab:$(TARGET_COPY_OUT_RECOVERY_ROOT)/etc/fstab \
     $(LOCAL_PATH)/recovery/root/init.recovery.platform.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.platform.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.qcom.rc \
     $(LOCAL_PATH)/recovery/root/init.recovery.svc.rc:$(TARGET_COPY_OUT_RECOVERY_ROOT)/init.recovery.svc.rc \
