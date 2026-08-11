2bac1aa636b0a53d80e53d24589a11eeac6c9e49
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-08-11 21:50:43 +0800

    fix: inline GuardianAngle Binder call directly in Decrypt.cpp - no gadecrypt binary needed

--- recovery/root/patch_decrypt.py
+++ recovery/root/patch_decrypt.py
@@ -6,17 +6,46 @@ shutil.copy(path, path + '.bak')
 with open(path, 'r') as f:
     content = f.read()
 
+# Add Binder include
+old_inc = '#include "Decrypt.h"'
+new_inc = '''#include "Decrypt.h"
+#include <binder/IServiceManager.h>
+#include <binder/Parcel.h>
+#include <utils/String16.h>'''
+assert old_inc in content, 'Decrypt.h include not found'
+content = content.replace(old_inc, new_inc, 1)
+
+# Add GuardianAngle call before spblob check
 old = '\tif (stat("/data/system_de/0/spblob", &st) == 0) {'
 
-new = '''\t// vivo CE decrypt: call gadecrypt (GuardianAngle Binder client)
-\tprintf("Vivo FBE decrypt attempt via gadecrypt...\\n");
-\tchar gacmd[256];
-\tsnprintf(gacmd, sizeof(gacmd), "/system/bin/gadecrypt %d '%s'", user_id, Password.c_str());
-\tif (system(gacmd) == 0) {
-\t\tprintf("gadecrypt returned success\\n");
-\t\treturn true;
+new = '''\t// vivo CE decrypt: call GuardianAngle Binder service directly
+\t{
+\t\tprintf("Vivo FBE decrypt: calling GuardianAngle...\\n");
+\t\tandroid::sp<android::IServiceManager> sm = android::defaultServiceManager();
+\t\tif (sm != nullptr) {
+\t\t\tandroid::sp<android::IBinder> binder = sm->getService(android::String16("GuardianAngleService"));
+\t\t\tif (binder != nullptr) {
+\t\t\t\tandroid::Parcel data, reply;
+\t\t\t\tdata.writeInterfaceToken(android::String16("android.IGuardianAngleService"));
+\t\t\t\tdata.writeInt32(user_id);
+\t\t\t\tdata.writeString16(android::String16(Password.c_str()));
+\t\t\t\tandroid::status_t status = binder->transact(1, data, &reply);
+\t\t\t\tif (status == android::NO_ERROR) {
+\t\t\t\t\tbool result = reply.readBool();
+\t\t\t\t\tprintf("guardian: verifyUserPassWord = %d\\n", result);
+\t\t\t\t\tif (result) return true;
+\t\t\t\t} else {
+\t\t\t\t\tprintf("guardian: transact failed: %d\\n", status);
+\t\t\t\t}
+\t\t\t} else {
+\t\t\t\tprintf("guardian: GuardianAngleService not found\\n");
+\t\t\t}
+\t\t} else {
+\t\t\tprintf("guardian: ServiceManager not available\\n");
+\t\t}
+\t\tprintf("guardian: falling back to standard decrypt\\n");
 \t}
-\tprintf("gadecrypt failed, trying standard methods\\n");
+
 ''' + old
 
 assert old in content, 'spblob check not found in Decrypt.cpp'
@@ -25,4 +54,4 @@ content = content.replace(old, new, 1)
 with open(path, 'w') as f:
     f.write(content)
 
-print('Decrypt.cpp patched for vivo CE decrypt via gadecrypt')
+print('Decrypt.cpp patched with inline GuardianAngle Binder call')
