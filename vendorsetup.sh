df039eea705c9967b5c1d5a80530fefd9bf3f4ff
hjiyu1965 <hjiyu1965@users.noreply.github.com>
2026-08-12 18:11:44 +0800

    cleanup: remove obsolete add_lunch_combo from vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,4 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-add_lunch_combo twrp_PD1936-user
-add_lunch_combo twrp_PD1936-userdebug
-add_lunch_combo twrp_PD1936-eng
+export ALLOW_MISSING_DEPENDENCIES=true
